e3900757c4c92608923e574f450ceb192ca275f6
omuben <omuben254@gmail.com>
2020-10-03 14:38:18 -0700

    edit

--- hellob/urls.py
+++ hellob/urls.py
@@ -1,4 +1,4 @@
-"""hellob URL Configuration
+"""ecommerce URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('myapp.urls')),
+    path('',include('myapp.urls'))
 ]
+
+if settings.DEBUG:
+    #urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
